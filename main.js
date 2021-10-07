3002901a1f3017487328d5d67cdfaa89ac9bf0f7
ReflextionsDev <ReflextionsDev@gmail.com>
2021-10-07 11:50:12 -0400

    complete assignment

--- main.js
+++ main.js
@@ -2,14 +2,79 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(first, last) {
+    const user = {
+        firstName: first,
+        lastName: last
+    }
+    return user
+}
+
+function setAge(user, num) {
+    user.age = num
+    return user
+}
+
+function incrementAge(user) {
+    user.age++
+    return user
+}
+
+function fixCar(car) {
+    car.needsMaitenance = false
+    return car
+}
 
 
+function addGrades(student, newgrades) {
+    for (const i of newgrades) {
+        student.grades.push(i)
+    }
+    return student
+}
 
+function getDataType(obj, key) {
+    return typeof obj[key]
+}
 
+function addTodo(arr, newItems) {
+    arr.push(newItems)
+    return arr
+}
 
+function addSong(pl, song) {
+    pl.duration += song.duration
+    pl.songs.push(song)
+    return pl
+}
 
+function updateReportCard(reportCard, newGrade) {
 
+    // Adds the new grade to the report card's grades
+    reportCard.grades.push(newGrade)
 
+    // Update lowest
+    if (newGrade < reportCard.lowestGrade) {
+        reportCard.lowestGrade = newGrade
+    }
+
+    // Update highest
+    if (newGrade > reportCard.highestGrade) {
+        reportCard.highestGrade = newGrade
+    }
+
+    // Update average
+    let gradeSum = 0, gradeAvg = 0
+
+    for (const i of reportCard.grades) {
+        gradeSum += i
+    }
+
+    gradeAvg = gradeSum / reportCard.grades.length
+    reportCard.averageGrade = gradeAvg
+
+    return reportCard
+}
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
